72b1469e9b0ba16743cefb74ebf2b8256c6f1b1a
nackwon <ahwk321@gmail.com>
2022-04-15 16:02:21 +0900

    1. 기존 Validation 모두 제거 후 재 지정 2. 전체 Validation check 메소드 추가 3. 숫자 3자리인지 체크 메소드 추가 4. 숫자 0 포함되는지 체크 메소드 추가 5. 문자가 아닌 숫자로 이루어져있는지 체크 메소드 추가 6. 중복되는 값이 존재하는지 체크 메소드 추가

--- src/main/java/baseball/model/BaseballValidation.java
+++ src/main/java/baseball/model/BaseballValidation.java
@@ -1,46 +1,90 @@
 package baseball.model;
 
-import java.util.Collections;
-import java.util.List;
-
 public class BaseballValidation {
 
+    private static int MAX_INPUT_NUMBER_LENGTH = 3;
+
+
     /**
-     * 3 자리 입력 체크
+     * 사용자 입력 시 입력 값 관련 Validation 체크
+     * @param inputNumber
      * @return
      */
-    public static boolean checkCommonNumbers(String inputNumber) {
+    public static boolean isCheckCommonNumber(String inputNumber) {
 
-        if(inputNumber.length() > 3) {
+        if (!isCheckWord(inputNumber)) {
+            return false;
+        }
+
+        if(!isCheckNumberLength(inputNumber)) {
+            return false;
+        }
+
+        if(isCheckZeroNumber(inputNumber)) {
+            return false;
+        }
+
+        if(isCheckDuplicateNumbers(inputNumber)) {
             return false;
         }
         return true;
     }
 
     /**
-     * 중복된 값이 존재하는지 체크
-     * @param inputNumberList
+     * 입력된 숫자 3자리 인지 체크
      * @return
      */
-    public static boolean checkDuplicateNumbers(List<Integer> inputNumberList, int number) {
-
-        if(inputNumberList.contains(number)) {
-            return false;
-        }
-        return true;
-    }
-    /**
-     * Game 재 시작 시 숫자 체크
-     * @param number
-     * @return
-     */
-    public static boolean checkGameRestartNumber(int number) {
-        
-        if(number == 1 || number == 2) {
+    public static boolean isCheckNumberLength(String inputNumber) {
+        if(inputNumber.length() == MAX_INPUT_NUMBER_LENGTH)
             return true;
-        }
+
         return false;
     }
 
+    /**
+     * 숫자 0 을 포함하는지 체크
+     * @param inputNumber : 입력된 3자리 숫자
+     * @return
+     */
+    public static boolean isCheckZeroNumber(String inputNumber) {
+        return inputNumber.contains("0");
+    }
 
+    /**
+     * 입력된 값이 숫자로 이루어져 있는지 체크
+     * @param inputNumber : 입력된 3자리 숫자
+     * @return
+     */
+    public static boolean isCheckWord(String inputNumber) {
+
+        int intSize = inputNumber.length();
+
+        for (int i=0; i<intSize; ++i) {
+            if(!Character.isDigit(inputNumber.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * 중복된 값이 존재하는지 체크
+     * @param inputNumber
+     * @return
+     */
+    public static boolean isCheckDuplicateNumbers(String inputNumber) {
+
+        String[] arrNumbers = inputNumber.split("");
+
+        int intSize = arrNumbers.length;
+
+        for (int i=0; i<intSize; ++i) {
+            if(arrNumbers[i].equals(arrNumbers[i+1])) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
