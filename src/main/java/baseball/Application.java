18ac702d67135e39f40a2c59e12319e920cc2665
nackwon <ahwk321@gmail.com>
2022-04-15 17:01:30 +0900

    1. game starter 메소드 추가 2. input user 메소드 추가 3. computer user 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,47 @@
 package baseball;
 
+import baseball.model.Baseball;
+import baseball.model.BaseballValidation;
+import baseball.model.baseballCommonUtil;
+import baseball.view.BaseballInputView;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public void baseballGameStarter() {
+
+    }
+
+    /**
+     * Computer Random Number Logic
+     * @return
+     */
+    public List<Integer> computerUser() {
+        Baseball baseball = new Baseball();
+
+        String sComputerNumbers = baseball.computerRandomNumbers();
+        if (BaseballValidation.isCheckCommonNumber(sComputerNumbers)) {
+            return baseballCommonUtil.convertList(sComputerNumbers);
+        }
+        return null;
+    }
+
+    /**
+     * User input Number Logic
+     * @return
+     */
+    public List<Integer> inputUser() {
+        String sInputNumber = BaseballInputView.userInputNumbers();
+
+        if (BaseballValidation.isCheckCommonNumber(sInputNumber)) {
+            return baseballCommonUtil.convertList(sInputNumber);
+        }
+        return null;
     }
 }
