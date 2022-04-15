92afccebe27de394b7d6cdbbf88cdf2a9d4e1db6
nackwon <ahwk321@gmail.com>
2022-04-15 16:00:41 +0900

    baseball output View 클래스 추가
    1. 사용자 입력 후 스트라이크 볼 결과 값 메소드 추가
    2. 사용자 입력 후 낫싱 결과 값 리턴 메소드 추가
    3. 모두 맞추었을 경우 MSG 리턴 메소드 추가

---
+++ src/main/java/baseball/view/BaseballOutputView.java
@@ -0,0 +1,46 @@
+package baseball.view;
+
+public class BaseballOutputView {
+
+    private static String BASEBALL_STRIKE = "스트라이크";
+    private static String BASEBALL_BALL = "볼";
+    private static String BASEBALL_NOTHING = "낫씽";
+    private static String BASEBALL_RESULT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+
+    /**
+     * 사용자 숫자 입력 후 결과 값 리턴하는 Method
+     * @param strikeCount : 스트라이크 갯수
+     * @param ballCount : 볼 갯수
+     * @return
+     */
+    public static String resultStrikeAndBallCount(int strikeCount, int ballCount) {
+
+        if(strikeCount != 0 && ballCount != 0) {
+            return strikeCount + BASEBALL_STRIKE + ballCount + BASEBALL_BALL;
+        }
+
+        if(strikeCount != 0) {
+            return strikeCount + BASEBALL_STRIKE;
+        }
+
+        if(ballCount != 0) {
+            return ballCount + BASEBALL_BALL;
+        }
+        return null;
+    }
+
+    /********************
+     * 승리 시 호출 Method
+     *******************/
+    public static void winningMSG() {
+        System.out.println(BASEBALL_RESULT);
+    }
+
+    /********************
+     * 낫씽 시 호출 Method
+     *******************/
+    public static void nothingMSG() {
+        System.out.println(BASEBALL_NOTHING);
+    }
+}
