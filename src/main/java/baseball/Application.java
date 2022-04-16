cdcf1c70067faa347e0c9f3e88e2e3d3f95b7971
nackwon <ahwk321@gmail.com>
2022-04-16 16:46:54 +0900

    1. baseballGameStarter 메소드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,28 +1,75 @@
 package baseball;
 
 import baseball.model.Baseball;
+import baseball.model.BaseballStrikeAndBallCount;
 import baseball.model.BaseballValidation;
 import baseball.model.baseballCommonUtil;
 import baseball.view.BaseballInputView;
+import baseball.view.BaseballOutputView;
 import org.junit.jupiter.params.ParameterizedTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 프로그램
+
+        List<Integer> computerNumberList = computerUser();
+
+        baseballGameStarter(computerNumberList);
     }
 
-    public void baseballGameStarter() {
+    public static void baseballGameStarter(List<Integer> computerNumberList) {
 
+        Application application = new Application();
+        List<Integer> inputUserNumberList = application.inputUser();
+
+        Map mpBallCount = application.baseballGameRule(inputUserNumberList, computerNumberList);
+
+        int strikeCount = (int) mpBallCount.get("strike");
+        int ballCount = (int) mpBallCount.get("ball");
+
+        if (0 != strikeCount || 0 != ballCount) {
+            BaseballOutputView.resultStrikeAndBallCount((int) mpBallCount.get("strike"), (int) mpBallCount.get("ball"));
+            baseballGameStarter(computerNumberList);
+        }
+        if ( 0 == strikeCount && 0 == ballCount) {
+            BaseballOutputView.nothingMSG();
+            baseballGameStarter(computerNumberList);
+        }
+        if (3 == strikeCount) {
+            BaseballOutputView.winningMSG();
+            application.gameRestart();
+        }
+    }
+
+    public Map baseballGameRule(List<Integer> inputUserNumberList, List<Integer> computerUserNumberList) {
+
+        BaseballStrikeAndBallCount strikeAndBallCount = new BaseballStrikeAndBallCount();
+
+        return strikeAndBallCount.countBallCount(inputUserNumberList, computerUserNumberList);
+    }
+
+    public void gameRestart() {
+
+        // 재시작
+        String sRestartCode = BaseballInputView.restartGame();
+
+        if ("1".equals(sRestartCode)) {
+            BaseballInputView.userInputNumbers();
+        }
+        if("2".equals(sRestartCode)) {
+            return;
+        }
     }
 
     /**
      * Computer Random Number Logic
      * @return
      */
-    public List<Integer> computerUser() {
+    public static List<Integer> computerUser() {
         Baseball baseball = new Baseball();
 
         String sComputerNumbers = baseball.computerRandomNumbers();
