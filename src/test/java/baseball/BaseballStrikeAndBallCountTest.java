fae9bb15f04325864670ad75e57c4693d018656d
nackwon <ahwk321@gmail.com>
2022-04-18 16:51:54 +0900

    test : refactor 로 Test 메소드 다시 작성

--- src/test/java/baseball/BaseballStrikeAndBallCountTest.java
+++ src/test/java/baseball/BaseballStrikeAndBallCountTest.java
@@ -6,27 +6,41 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
 
 
 public class BaseballStrikeAndBallCountTest {
 
     @Test
-    @DisplayName("Strike Ball Count Test")
-    public void 스트라이크_볼_결과_테스트() {
+    @DisplayName("Strike  Count Test")
+    public void 스트라이크_카운트_결과_테스트() {
 
         String sUserNumber = "497";
-        String sComputerNumber = "497";
+        String sComputerNumber = "479";
         BaseballStrikeAndBallCount baseballStrikeAndBallCount = new BaseballStrikeAndBallCount();
 
         List<Integer> testUserList = baseballCommonUtil.convertList(sUserNumber);
         List<Integer> testComputerList = baseballCommonUtil.convertList(sComputerNumber);
 
-        Map mpResult = baseballStrikeAndBallCount.countBallCount(testUserList, testComputerList);
+        int intResult = baseballStrikeAndBallCount.countStrike(testUserList, testComputerList);
 
-        assertThat(mpResult).contains( entry("ball", 0), entry("strike", 3));
+        assertThat(intResult).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Ball count Test")
+    public void 볼_카운트_결과_테스트() {
+
+        String sUserNumber = "497";
+        String sComputerNumber = "947";
+        BaseballStrikeAndBallCount baseballStrikeAndBallCount = new BaseballStrikeAndBallCount();
+
+        List<Integer> testUserList = baseballCommonUtil.convertList(sUserNumber);
+        List<Integer> testComputerList = baseballCommonUtil.convertList(sComputerNumber);
+
+        int intResult = baseballStrikeAndBallCount.countBall(testUserList, testComputerList);
+
+        assertThat(intResult).isEqualTo(3);
     }
 }
