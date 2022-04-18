af3c07b62ab4035e2f1443f9a031ac6cd920fdc3
nackwon <ahwk321@gmail.com>
2022-04-18 16:43:40 +0900

    refactor : Map 으로 받아 처리했던 Strike , Ball 갯수를 따로 빼서 Rule 메소드를 만들어 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,6 +19,7 @@ public class Application {
 
         do {
             List<Integer> computerNumberList = computerUser();
+            System.out.println(computerNumberList);
             baseballGameStarter(computerNumberList);
         } while (gameRestart());
     }
@@ -28,35 +29,44 @@ public class Application {
         Application application = new Application();
         List<Integer> inputUserNumberList = application.inputUser();
 
-        Map mpBallCount = application.baseballGameRule(inputUserNumberList, computerNumberList);
-
-        int strikeCount = (int) mpBallCount.get("strike");
-        int ballCount = (int) mpBallCount.get("ball");
-
-        if ( 0 == strikeCount && 0 == ballCount) {
-            BaseballOutputView.nothingMSG();
-            baseballGameStarter(computerNumberList);
-            return;
-        }
-
-        if (3 == strikeCount) {
-            BaseballOutputView.resultStrikeAndBallCount(strikeCount, ballCount);
-            BaseballOutputView.winningMSG();
-            return;
-        }
-
-        if( 0 != strikeCount || 0 != ballCount) {
-            BaseballOutputView.resultStrikeAndBallCount(strikeCount, ballCount);
-            baseballGameStarter(computerNumberList);
-            return;
-        }
-    }
-
-    public Map baseballGameRule(List<Integer> inputUserNumberList, List<Integer> computerUserNumberList) {
         BaseballStrikeAndBallCount strikeAndBallCount = new BaseballStrikeAndBallCount();
-        return strikeAndBallCount.countBallCount(inputUserNumberList, computerUserNumberList);
+
+        int intStrikeCount = strikeAndBallCount.countStrike(inputUserNumberList, computerNumberList);
+        int intBallCount = strikeAndBallCount.countBall(inputUserNumberList, computerNumberList);
+
+        if(baseballGameRule(intStrikeCount, intBallCount)) {
+            baseballGameStarter(computerNumberList);
+        }
     }
 
+    /**
+     * 
+     * @param intStrikeCount : 스트라이크 갯수
+     * @param intBallCount : 볼의 갯수
+     * @return
+     */
+    public static boolean baseballGameRule(int intStrikeCount, int intBallCount) {
+
+        if(intStrikeCount == 3) {
+            BaseballOutputView.resultStrikeAndBallCount(intStrikeCount, intBallCount - intStrikeCount);
+            BaseballOutputView.winningMSG();
+            return false;
+        }
+        if(0 == intStrikeCount && 0 == intBallCount) {
+            BaseballOutputView.nothingMSG();
+            return true;
+        }
+        if(0 != intStrikeCount || 0 != intBallCount) {
+            BaseballOutputView.resultStrikeAndBallCount(intStrikeCount, intBallCount - intStrikeCount);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Game Restart Method
+     * @return
+     */
     public static boolean gameRestart() {
 
         final int NEW_GAME_CODE = 1;
